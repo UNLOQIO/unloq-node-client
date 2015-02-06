dad8a22c0acacdf4bf6b95803b8914c20c4ec977
snupa <bunta.adrian@gmail.com>
2015-02-06 18:15:05 +0200

    - updated authentication & token confirmation endpoints

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 /*
 * These are our components that we're going to expose.
 * */
-var bluebird = require('bluebird'),
+var EventEmitter = require('events').EventEmitter,
+  bluebird = require('bluebird'),
   AuthClass = require('./lib/auth.js'),
   ApiClass = require('./lib/api.js');
+
 
 /* First thing we do is we wrap the "promise" function over bluebird. */
 bluebird.promise = function CreateShieldPromise(handler) {
   var _resolve,
-    _reject;
+    _reject,
+    eventObj = new EventEmitter();
   var pObj = new bluebird(function(resolve, reject) {
     _resolve = resolve;
     _reject = reject;
-    try {
-      handler(resolve, reject);
-    } catch(e) {
-      reject(e);
-    }
+    process.nextTick(function() {
+      try {
+        handler.call(pObj, resolve, reject);
+      } catch(e) {
+        reject(e);
+      }
+    });
   });
   pObj.resolve = _resolve;
   pObj.reject = _reject;
   pObj.success = pObj.done;
+  pObj.on = function OnEvent() {
+    eventObj.on.apply(eventObj, arguments);
+    return pObj;
+  };
+  pObj.emit = function EmitEvent() {
+    eventObj.emit.apply(eventObj, arguments);
+    return pObj;
+  };
+  pObj.once = function OnSingleEvent() {
+    eventObj.once.apply(eventObj, arguments);
+    return pObj;
+  };
   return pObj;
 };
 
